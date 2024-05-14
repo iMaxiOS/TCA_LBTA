03bbc28d1a329b59553981e6f1ec761f6f06dc10
Maxim H <ukr.maksim1@gmail.com>
2024-05-14 12:07:22 +0300

    first push

---
+++ TCA_LBTA/TCA_LBTA/TCA_LBTAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_LBTAApp.swift
+//  TCA_LBTA
+//
+//  Created by iMac on 14.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct TCA_LBTAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
